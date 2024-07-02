972e16acbd136ceff273de5dfc7be52d5cb3c18b
Damis Bachtiar <damis.bachtiar@gmail.com>
2024-07-02 10:01:55 +0700

    feat: simple calculator

--- src/views/SimpleCalculatorPage.vue
+++ src/views/SimpleCalculatorPage.vue
@@ -9,13 +9,26 @@
             </ion-toolbar>
         </ion-header>
         <ion-content :fullscreen="true">
-            <ion-header collapse="condense">
-                <ion-toolbar>
-                    <ion-title size="large">Simple Calculator</ion-title>
-                </ion-toolbar>
-            </ion-header>
             <div id="container">
-                <strong class="capitalize">Simple Calculator</strong>
+                <ion-input label="Number input" type="number" placeholder="0" v-model="var1"></ion-input>
+                <ion-input label="Number input" type="number" placeholder="0" v-model="var2"></ion-input>
+                <ion-grid>
+                    <ion-row>
+                        <ion-col>
+                            <ion-button @click="tambah">+</ion-button>
+                        </ion-col>
+                        <ion-col>
+                            <ion-button @click="kurang">-</ion-button>
+                        </ion-col>
+                        <ion-col>
+                            <ion-button @click="kali">*</ion-button>
+                        </ion-col>
+                        <ion-col>
+                            <ion-button @click="bagi">/</ion-button>
+                        </ion-col>
+                    </ion-row>
+                </ion-grid>
+                <ion-input label="Result" type="number" placeholder="0" v-model="result"></ion-input>
             </div>
         </ion-content>
     </ion-page>
@@ -29,6 +42,43 @@
       IonToolbar,
       IonPage,
       IonButtons,
-      IonMenuButton
+      IonMenuButton,
+      IonCol, 
+      IonGrid, 
+      IonRow,
+      IonInput,
+      IonButton
     } from '@ionic/vue';
+    import {ref} from 'vue';
+
+    const var1 = ref(0);
+    const var2 = ref(0);
+    const result = ref(0);
+
+    function tambah() {
+        result.value = var1.value + var2.value;
+    }
+
+    function kurang() {
+        result.value = var1.value - var2.value;
+    }
+
+    function kali() {
+        result.value = var1.value * var2.value;
+    }
+
+    function bagi() {
+        result.value = var1.value / var2.value;
+    }
 </script>
+<style scoped>
+    #container {
+        text-align: center;
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 50%;
+        padding: 20%;
+        transform: translateY(-50%);
+    }
+</style>
